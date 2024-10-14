f0b1699d39e54eeb432272a482fe2fdc940c963b
PTParra <49121638+PTParra@users.noreply.github.com>
2024-10-14 12:08:23 +0200

    Verdadero commit inicial

---
+++ app.js
@@ -0,0 +1,18 @@
+/*
+    We all know about Roman Numerals, and if not, here's a nice introduction kata. And if you were anything like me, you 'knew' that 
+    the numerals were not used for zeroes or fractions; but not so!
+
+    I learned something new today: the Romans did use fractions and there was even a glyph used to indicate zero.
+
+    So in this kata, we will be implementing Roman numerals and fractions.
+
+    Although the Romans used base 10 for their counting of units, they used base 12 for their fractions.
+
+    Further, zero was represented by N.
+
+    Complete the method that takes two parameters: an integer component in the range 0 to 5000 inclusive, and an optional 
+    fractional component in the range 0 to 11 inclusive.
+
+    You must return a string with the encoded value. Any input values outside the ranges given above should return "NaR" 
+    (i.e. "Not a Roman" :-)
+*/
